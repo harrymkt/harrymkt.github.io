96a6e7aefecebd6d5fb951738f7dfcea3a70792c
harrymkt <harrymkt@users.noreply.github.com>
2024-08-27 15:41:54 +0000

    deploy: 80b88993891e449fd3650b2fcba458d871075e1e

---
+++ scripts/all.js
@@ -0,0 +1,5 @@
+//all.js
+var birthyear = 2006; //the variable of the year which harry min khant born.
+var ptype="https"; //the protercall type, http/"+ptype+".
+var domain="harrymkt.github.io"; //domain.
+var ownername="Harry Min Khant";
