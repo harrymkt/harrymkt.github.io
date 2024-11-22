bd3b990991b512cb00706b4c13bc2e44c96815d0
Harry Min Khant <118270468+harrymkt@users.noreply.github.com>
2024-11-22 15:47:46 +0630

    GH

--- content/blog/_index.md
+++
@@ -1,9 +0,0 @@
-+++
-title = "Blog"
-template = "section_paginated.html"
-page_template = "blogpage.html"
-sort_by = "update_date"
-paginate_by = 5
-+++
-# Blog
-Here I will share my useful posts
--- static/scripts/all.js
+++
@@ -1,5 +0,0 @@
-//all.js
-var birthyear = 2006; //the variable of the year which harry min khant born.
-var ptype="https"; //the protercall type, http/"+ptype+".
-var domain="harrymkt.github.io"; //domain.
-var ownername="Harry Min Khant";
